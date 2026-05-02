68d78d16943ebc7ed59d1261c603dec34c7a83bc
Uday Manchanda <uday.manchanda14@gmail.com>
2026-05-02 17:54:55 -0500

    fix

--- commands/dudewtf.js
+++ commands/dudewtf.js
@@ -3,6 +3,6 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
     data: new SlashCommandBuilder().setName('dudewtf').setDescription('dude wtf'),
     async execute(interaction) {
-        await interaction.reply('https://cdn.discordapp.com/attachments/1307965555520831530/1500267759026835647/Family_Guy_-_Who_the_fk_starts_a_conversation_like_that_i_just_sat_down.mp4?ex=69f7d0bb&is=69f67f3b&hm=4434fbb6c56fd16dbda64870373f378c9ddf1cb81d4af056b5fd80b63de708bc&');
+        await interaction.reply('https://youtu.be/MmzJ-deqBtg?si=7XDHN8KT_14ReguP');
     },
 };
